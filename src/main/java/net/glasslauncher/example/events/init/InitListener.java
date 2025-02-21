26e171f1051e5a9fadfe55e4cb42dcb989bd0021
telvarost <telontelvarost@gmail.com>
2025-02-21 18:03:35 -0500

    Alternate fix to keep event listener private

--- src/main/java/net/glasslauncher/example/events/init/InitListener.java
+++ src/main/java/net/glasslauncher/example/events/init/InitListener.java
@@ -3,18 +3,25 @@ package net.glasslauncher.example.events.init;
 import net.mine_diver.unsafeevents.listener.EventListener;
 import net.modificationstation.stationapi.api.event.mod.InitEvent;
 import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
+import net.modificationstation.stationapi.api.mod.entrypoint.EntrypointManager;
 import net.modificationstation.stationapi.api.util.Namespace;
 import net.modificationstation.stationapi.api.util.Null;
 import org.apache.logging.log4j.Logger;
 
+import java.lang.invoke.MethodHandles;
+
 public class InitListener {
+    static {
+        EntrypointManager.registerLookup(MethodHandles.lookup());
+    }
+
     @SuppressWarnings("UnstableApiUsage")
     public static final Namespace NAMESPACE = Namespace.resolve();
 
     public static final Logger LOGGER = NAMESPACE.getLogger();
 
     @EventListener
-    public static void serverInit(InitEvent event) {
+    private static void serverInit(InitEvent event) {
         LOGGER.info(NAMESPACE.toString());
     }
 }
